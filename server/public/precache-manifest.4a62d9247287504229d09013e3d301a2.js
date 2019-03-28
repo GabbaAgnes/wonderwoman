d938b7f94b45e05ecf548de4ce00b5b7b65460ff
Agnes <gabbaagnes@gmail.com>
2019-03-27 20:38:57 -0700

    yesgood

---
+++ server/public/precache-manifest.4a62d9247287504229d09013e3d301a2.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "da9429bcb17cffefd3dda1d22f1a48bd",
+    "url": "/static/media/1.da9429bc.png"
+  },
+  {
+    "revision": "c2c665994d1bac884179",
+    "url": "/static/css/main.aa441026.chunk.css"
+  },
+  {
+    "revision": "efc4de0a6ec7242b60ac",
+    "url": "/static/js/1.efc4de0a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e36836296b5cfe0d70a791edab3f6eea",
+    "url": "/static/media/photo2.e3683629.jpg"
+  },
+  {
+    "revision": "5bea5e1bec1faf298e851b2e2288fc1b",
+    "url": "/static/media/try5.5bea5e1b.gif"
+  },
+  {
+    "revision": "4b5ee6e8a6b7f024b2c7e98b2a891a74",
+    "url": "/static/media/2.4b5ee6e8.jpg"
+  },
+  {
+    "revision": "79c9254a8cf3a1a8935aca568f50cd6a",
+    "url": "/static/media/pause.79c9254a.jpg"
+  },
+  {
+    "revision": "25ae1178518b2be551a43d1b9ba9126e",
+    "url": "/static/media/11.25ae1178.jpg"
+  },
+  {
+    "revision": "20242c0aaabb9199b399612928ee212a",
+    "url": "/static/media/read.20242c0a.jpg"
+  },
+  {
+    "revision": "ff9ca99f879cd3b3a72badc999179c16",
+    "url": "/static/media/sunken.ff9ca99f.jpg"
+  },
+  {
+    "revision": "9554d8efa215d4d18ec33b0ad940f84d",
+    "url": "/static/media/endow.9554d8ef.gif"
+  },
+  {
+    "revision": "d5fb0cc93be342890114d0c25059d558",
+    "url": "/static/media/tradeoff1.d5fb0cc9.jpg"
+  },
+  {
+    "revision": "6668b12eebb89e761b3e5a2ca31c5028",
+    "url": "/static/media/focus.6668b12e.gif"
+  },
+  {
+    "revision": "e049c162538a3c90b257babb2a51f98e",
+    "url": "/static/media/play.e049c162.jpg"
+  },
+  {
+    "revision": "a12c8107415ad5e6bcbbee5a77646cc3",
+    "url": "/static/media/11.a12c8107.png"
+  },
+  {
+    "revision": "8a3733cc4440989c8b1d541084076a2d",
+    "url": "/static/media/angel.8a3733cc.jpg"
+  },
+  {
+    "revision": "044ad3de01467b119e8e552a353b3dee",
+    "url": "/static/media/train.044ad3de.png"
+  },
+  {
+    "revision": "c2c665994d1bac884179",
+    "url": "/static/js/main.c2c66599.chunk.js"
+  },
+  {
+    "revision": "322418e2fd79d02f4f393669da9dd13e",
+    "url": "/static/media/5.322418e2.png"
+  },
+  {
+    "revision": "9064caa0862f1e61a95d2c7943e37233",
+    "url": "/static/media/edit.9064caa0.png"
+  },
+  {
+    "revision": "813d57ee3e8a8042e142ed0e3fbdfa62",
+    "url": "/static/media/whatif1.813d57ee.gif"
+  },
+  {
+    "revision": "3d1cf3e88f113293bd60969267660a4b",
+    "url": "/static/media/sleep.3d1cf3e8.png"
+  },
+  {
+    "revision": "2e45d2ab461838f16eabc6795bb4d161",
+    "url": "/static/media/ty4.2e45d2ab.gif"
+  },
+  {
+    "revision": "e20cdf01e087c7d00dd8f44bfd7392dd",
+    "url": "/static/media/soon.e20cdf01.png"
+  },
+  {
+    "revision": "576208abb604ee39092d8783b348301a",
+    "url": "/static/media/stretched.576208ab.gif"
+  },
+  {
+    "revision": "b0307ec77f435e8efb83ce2f6716c4a8",
+    "url": "/static/media/grace.b0307ec7.png"
+  },
+  {
+    "revision": "095c836934d143d2495ae6f75717fb70",
+    "url": "/static/media/8.095c8369.png"
+  },
+  {
+    "revision": "a27ce47599419d919dceb8125273bdbc",
+    "url": "/static/media/6.a27ce475.png"
+  },
+  {
+    "revision": "5951c33c435ec6757e6f34d3ddb93527",
+    "url": "/static/media/try6.5951c33c.gif"
+  },
+  {
+    "revision": "30803551ac2245736a76dfc62ba5b456",
+    "url": "/static/media/9.30803551.png"
+  },
+  {
+    "revision": "0699afeeb54be996e55360c0e214effc",
+    "url": "/static/media/no.0699afee.png"
+  },
+  {
+    "revision": "d92150466c03bada33eff48a5426916f",
+    "url": "/static/media/7.d9215046.png"
+  },
+  {
+    "revision": "6d05f2a21ced92a491c894fdc9131c3c",
+    "url": "/static/media/10.6d05f2a2.png"
+  },
+  {
+    "revision": "f99982d4fc8d29bcd60002b19faa384e",
+    "url": "/static/media/reborn.f99982d4.png"
+  },
+  {
+    "revision": "180fd203a230925af90f36264e851d12",
+    "url": "/static/media/6ee.180fd203.gif"
+  },
+  {
+    "revision": "3024a81eb81083bd45d3963d84b328b1",
+    "url": "/static/media/try7.3024a81e.gif"
+  },
+  {
+    "revision": "f79c0a95c9d68c52c0633357fcbf124d",
+    "url": "/index.html"
+  }
+];
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.51a2c0d2f882a15ff8310678f7ed6981.js"
+  "/precache-manifest.4a62d9247287504229d09013e3d301a2.js"
 );
 
 workbox.clientsClaim();
